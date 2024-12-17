bd2ee2f250b8ffa786212ea3a7be82573b270e7f
skhare <skhare@argusoft.com>
2024-12-17 15:46:41 +0530

    data storage in flutter and api to send data

--- app/src/main/java/com/example/dummy_android/MainActivity.java
+++ app/src/main/java/com/example/dummy_android/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.dummy_android;
 
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,18 +15,35 @@ import org.json.JSONObject;
 
 import java.io.InputStream;
 
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE_GET_JSON = 1;
     private static final int REQUEST_CODE_FLUTTER_ACTIVITY = 2;
+    private static final String CHANNEL = "com.example.channel";
+    private FlutterEngine flutterEngine;
+    private MethodChannel methodChannel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        flutterEngine = new FlutterEngine(this);
+        flutterEngine.getDartExecutor().executeDartEntrypoint(
+                io.flutter.embedding.engine.dart.DartExecutor.DartEntrypoint.createDefault()
+        );
+        methodChannel = new MethodChannel(flutterEngine.getDartExecutor(), CHANNEL);
+
         Button btnOpenOpenSRPForm = findViewById(R.id.btn_open_opensrp_form);
         Button btnOpenFlutterActivity = findViewById(R.id.btn_open_flutter_activity);
+        Button btnStoreData = findViewById(R.id.btn_store_data);
+
+        Button sendDataViaApi = findViewById(R.id.send_data_via_api);
+
+        Button btnGetData = findViewById(R.id.btn_get_data);
 
         btnOpenOpenSRPForm.setOnClickListener(v -> {
             String formJson = getAutoPopulatedJsonForm();
@@ -52,13 +67,80 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("age", "30");
             startActivityForResult(intent, REQUEST_CODE_FLUTTER_ACTIVITY);
         });
+
+        btnStoreData.setOnClickListener(v -> {
+            Log.d("MainActivity", "Store Data button clicked");
+            storeDataInFlutter();
+        });
+
+        btnGetData.setOnClickListener(v -> {
+            Log.d("MainActivity", "Get Data button clicked");
+            getAllUsersFromFlutter();
+        });
+        sendDataViaApi.setOnClickListener(v -> {
+            Log.d("MainActivity", "Send Data via api button clicked");
+            sendDataViaApi();
+        });
+    }
+    private void storeDataInFlutter() {
+        methodChannel.invokeMethod("storeDataInFlutter", null, new MethodChannel.Result() {
+            @Override
+            public void success(Object result) {
+                Log.d("MainActivity", "Data stored successfully in Flutter DB.");
+            }
+
+            @Override
+            public void error(String errorCode, String errorMessage, Object errorDetails) {
+                Log.e("MainActivity", "Error storing data: " + errorMessage);
+            }
+
+            @Override
+            public void notImplemented() {
+                Log.e("MainActivity", "storeDataInFlutter method not implemented in Flutter");
+            }
+        });
     }
 
-    /**
-     * Loads and returns the JSON form string from the assets folder.
-     *
-     * @return The JSON form as a String.
-     */
+    private void getAllUsersFromFlutter() {
+        methodChannel.invokeMethod("getAllUsers", null, new MethodChannel.Result() {
+            @Override
+            public void success(Object result) {
+                Log.d("MainActivity", "Users fetched successfully: " + result);
+            }
+
+            @Override
+            public void error(String errorCode, String errorMessage, Object errorDetails) {
+                Log.e("MainActivity", "Error fetching users: " + errorMessage);
+            }
+
+            @Override
+            public void notImplemented() {
+                Log.e("MainActivity", "getAllUsers method not implemented in Flutter");
+            }
+        });
+    }
+
+    private void sendDataViaApi() {
+        methodChannel.invokeMethod("sendDataViaApi", null, new MethodChannel.Result() {
+            @Override
+            public void success(Object result) {
+                Log.d("MainActivity", "Data sent successfully via API.");
+            }
+
+            @Override
+            public void error(String errorCode, String errorMessage, Object errorDetails) {
+                Log.e("MainActivity", "Error sending data via API: " + errorMessage);
+            }
+
+            @Override
+            public void notImplemented() {
+                Log.e("MainActivity", "sendDataViaApi method not implemented in Flutter");
+            }
+        });
+    }
+
+    // Loads and returns the JSON form string from the assets folder.
+    //return The JSON form as a String
     private String getAutoPopulatedJsonForm() {
         try {
             InputStream inputStream = getAssets().open("forms/sample_form.json");
@@ -73,11 +155,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * Starts the JsonFormActivity with the provided JSON form.
-     *
-     * @param formJson The JSON form string.
-     */
+
+    //Starts the JsonFormActivity with the provided JSON form.
+    // param formJson The JSON form string.
+
     private void startJsonFormActivity(String formJson) {
         try {
             Intent intent = new Intent(this, JsonFormActivity.class);
--- app/src/main/java/com/example/dummy_android/MyFlutterActivity.java
+++ app/src/main/java/com/example/dummy_android/MyFlutterActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import io.flutter.embedding.android.FlutterActivity;
@@ -15,29 +16,120 @@ import io.flutter.plugin.common.MethodChannel;
 public class MyFlutterActivity extends FlutterActivity {
 
     private static final String CHANNEL = "com.example.channel";
+    private static MethodChannel methodChannel;
 
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
         super.configureFlutterEngine(flutterEngine);
 
-        new MethodChannel(flutterEngine.getDartExecutor(), "com.example.channel")
-                .setMethodCallHandler((call, result) -> {
-                    if (call.method.equals("submitFormJson")) {
-                        // Retrieve form data from Flutter
-                        Map<String, String> formData = call.arguments();
-                        Log.d("MyFlutterActivity", "Received form data: " + formData.toString());
+        methodChannel = new MethodChannel(flutterEngine.getDartExecutor(), CHANNEL);
 
-                        // Process the data or pass it back to the MainActivity, if needed
-                        Intent intent = new Intent();
-                        for (String key : formData.keySet()) {
-                            intent.putExtra(key, formData.get(key));
-                        }
-                        setResult(RESULT_OK, intent);
-                        finish();
-                    } else {
-                        result.notImplemented();
+        methodChannel.setMethodCallHandler((call, result) -> {
+            if (call.method.equals("submitFormJson")) {
+                Map<String, String> formData = call.arguments();
+                if (formData != null) {
+                    Map<String, String> processedData = new HashMap<>();
+                    for (Map.Entry<String, String> entry : formData.entrySet()) {
+                        processedData.put(entry.getKey(), entry.getValue());
                     }
-                });
+                    Log.d("MyFlutterActivity", "Processed form data: " + processedData);
+
+                    Intent intent = new Intent();
+                    for (String key : processedData.keySet()) {
+                        intent.putExtra(key, processedData.get(key));
+                    }
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+
+            }else if (call.method.equals("getAllUsers")) {
+                result.success(null);
+            }else if (call.method.equals("storeDataInFlutter")) {
+                result.success(null);
+            }else if (call.method.equals("sendDataViaApi")) {
+                result.success(null);
+            } else {
+                result.notImplemented();
+            }
+        });
     }
 
+    /**
+     * Static method to invoke the "getAllUsers" method on Flutter.
+     */
+    public static void getAllUsersFromFlutter() {
+        if (methodChannel != null) {
+            methodChannel.invokeMethod("getAllUsers", null, new MethodChannel.Result() {
+                @Override
+                public void success(Object result) {
+                    if (result instanceof List) {
+                        List<Map<String, Object>> users = (List<Map<String, Object>>) result;
+                        for (Map<String, Object> user : users) {
+                            Log.d("MyFlutterActivity", "User from Flutter DB: " + user);
+                        }
+                    } else {
+                        Log.e("MyFlutterActivity", "Unexpected result format: " + result);
+                    }
+                }
+
+                @Override
+                public void error(String errorCode, String errorMessage, Object errorDetails) {
+                    Log.e("MyFlutterActivity", "Error fetching users: " + errorMessage);
+                }
+
+                @Override
+                public void notImplemented() {
+                    Log.e("MyFlutterActivity", "getAllUsers method not implemented in Flutter");
+                }
+            });
+        } else {
+            Log.e("MyFlutterActivity", "MethodChannel is not initialized");
+        }
+    }
+
+    public static void storeDataInFlutter() {
+        if (methodChannel != null) {
+            methodChannel.invokeMethod("storeDataInFlutter", null, new MethodChannel.Result() {
+                @Override
+                public void success(Object result) {
+                    System.out.println("MyFlutterActivity"+ "-------------------------- " + result);
+                }
+
+                @Override
+                public void error(String errorCode, String errorMessage, Object errorDetails) {
+                    Log.e("MyFlutterActivity", "Error fetching users: " + errorMessage);
+                }
+
+                @Override
+                public void notImplemented() {
+                    Log.e("MyFlutterActivity", "getAllUsers method not implemented in Flutter");
+                }
+            });
+        } else {
+            Log.e("MyFlutterActivity", "MethodChannel is not initialized");
+        }
+    }
+
+    public static void sendDataViaApi() {
+        if (methodChannel != null) {
+            methodChannel.invokeMethod("sendDataViaApi", null, new MethodChannel.Result() {
+                @Override
+                public void success(Object result) {
+                    System.out.println("MyFlutterActivity"+ "-------------------------- " + result);
+                }
+
+                @Override
+                public void error(String errorCode, String errorMessage, Object errorDetails) {
+                    Log.e("MyFlutterActivity", "Error sending data: " + errorMessage);
+                }
+
+                @Override
+                public void notImplemented() {
+                    Log.e("MyFlutterActivity", "sendDataViaApi method not implemented in Flutter");
+                }
+            });
+        } else {
+            Log.e("MyFlutterActivity", "MethodChannel is not initialized");
+        }
+    }
 }
